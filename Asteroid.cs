9ec65d54e0599c592254db45bf5c08be54a513d8
tiagosutter <tiagosutter@gmail.com>
2025-03-16 15:51:41 -0300

    Moves asteroids spawn offscreen and adds randomness to vertical position

--- Asteroid.cs
+++ Asteroid.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace Spaceship;
 
 public class Asteroid
 {
     public const int BaseSpeed = 250;
-    public Vector2 Position = new(600, 300);
+    public Vector2 Position;
     private readonly int _speed;
     private const float Radius = 59;
 
     public Asteroid(int speed)
     {
+        var random = new Random();
+        Position = new(1380, random.Next(0, 721));
         _speed = speed;
     }
 
